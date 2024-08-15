d27dbd1300d9c5c91dc9a6098d18664f7d9e008c
JHON REYNOSO <jramirez@arkusnexus.com>
2024-08-15 09:29:43 -0400

    cleaning code, adding small comment

--- ContactsApi/ConfigureServices.cs
+++ ContactsApi/ConfigureServices.cs
@@ -8,9 +8,7 @@ public static class ConfigureServices
 
     public static IServiceCollection AddWebApiServices(this IServiceCollection services)
     {
-
-
-
+        //for demo.. here whould be services registered from web api
         return services;
     }
 }
